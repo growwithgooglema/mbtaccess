d5a0381250843bf4efd3a76f44d18ab2c9a5542c
Abdou Seck <aseck@mit.edu>
2018-04-08 09:24:24 -0400

    minor edits to school.js

--- school.js
+++ school.js
@@ -1,11 +1,15 @@
 // This is supposed to script to define a Place class/prototype
 
 let https = require('https');
-
+// import https from 'https';
 const token = process.env.GMAPS_API;
 
+let universities = require('../../test_scripts/universities.json').map(school => {
+  return school.name;
+});
+
 class Place {
-  constructor({ name = null, street = null, placeID = null, lat = null, long = null } = {}) {
+  constructor ({name = null, street = null, placeID = null, lat = null, long = null} = {}) {
     this.name = name;
     this.street = street;
     this.placeID = placeID;
@@ -13,78 +17,82 @@ class Place {
     this.long = long;
   }
 
-  getStreet() {
+  getStreet () {
     return this.street;
   }
 
-  getLat() {
+  getLat () {
     return this.lat;
   }
 
-  getLong() {
+  getLong () {
     return this.long;
   }
 
-  getPlaceID() {
+  getPlaceID () {
     return this.placeID;
   }
 
-  setStreet(val) {
+  setStreet (val) {
     this.street = val;
   }
 
-  setLat(val) {
+  setLat (val) {
     this.lat = val;
   }
 
-  setLong(val) {
+  setLong (val) {
     this.long = val;
   }
 
-  setPlaceID(val) {
+  setPlaceID (val) {
     this.placeID = val;
   }
 
-  populateAttributes() {
+  populateAttributes () {
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.name.replace(' ', '+')}&key=${token}`;
     https.get(url, response => {
-      let body = "";
-      response.on("data", data => {
+      let body = '';
+      response.on('data', data => {
         body += data;
       });
-      response.on("end", () => {
+      response.on('end', () => {
         body = JSON.parse(body);
-        this.setStreet(body.results[0].formatted_address);
-        this.setPlaceID(body.results[0].place_id);
-        this.setLat(body.results[0].geometry.location.lat);
-        this.setLong(body.results[0].geometry.location.lng);
+        if (body.results.length) {
+          this.setStreet(body.results[0].formatted_address);
+          this.setPlaceID(body.results[0].place_id);
+          this.setLat(body.results[0].geometry.location.lat);
+          this.setLong(body.results[0].geometry.location.lng);
+        }
         // console.log(this);
       });
-    }).on("error", (e) => {
+    }).on('error', (e) => {
       console.error(e);
     });
     // console.log(this);
   }
 
   // Get the MBTA bus stops within a 1 mile radius
-  getStops() {
+  getStops () {
     // Point this call to an API with lat=this.lat&long=this.long&radius=1
+    // To be implemented
   }
 
-  toString() {
+  toString () {
     return `Name: ${this.name}\nStreet Address: ${this.street}\nLatitute: ${this.lat} N\nLongitude: ${this.long} W.`;
   }
 }
 
-let mit = new Place({
-  name: 'Massachusetts Institute of Technology',
-  street: '77 Massachusetts Avenue, Cambridge, MA 02139',
-  lat: 42.36,
-  long: -71.092
-});
+// The issue (maybe good thing actually) with https or http is that requests are asynchronous.
+// This means that after calling Place.populateAttributes(), you have to time out a little bit
+// to wait for the method to finish running in order to see the changes made.
 
-mit.populateAttributes();
+let updatedUniversities = universities.map(name => {
+  let place = new Place({name: name});
+  place.populateAttributes();
+  return place;
+});
+// mit.populateAttributes();
 setTimeout(function () {
-  console.log(mit);
-}, 1000);
-console.log(mit);
+  console.log(JSON.stringify(updatedUniversities));
+}, 2000);
