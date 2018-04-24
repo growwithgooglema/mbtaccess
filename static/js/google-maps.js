83cdea9a63a711b0195b31046f00933317d8fe0b
Abdou Seck <djily02016@gmail.com>
2018-04-23 22:59:41 -0400

    show markers for stops on the map, instead of searched location

--- static/js/google-maps.js
+++ static/js/google-maps.js
@@ -1,21 +1,24 @@
 // Google maps code to generate a map and populate it with
 // info related to the end user's requested location
 
-// This is a very lazy way of creating a Place object
-var userLocation = {
-  name: 'Your Current Location',
-  formatted_address: '',
-  geometry: {
-    location: {
-      lat: function() {
-        return null;
-      },
-      lng: function() {
-        return null;
+class Place {
+  constructor({name="Your Current Location", formatted_address="", lat=null, lng=null, wheelchair_accessible=0} = {}) {
+    this.name = name;
+    this.formatted_address = formatted_address;
+    this.wheelchair_accessible = wheelchair_accessible;
+    this.geometry = {
+      location: {
+        lat: function() {
+          return lat;
+        },
+        lng: function() {
+          return lng;
+        }
       }
-    }
+    };
   }
-};
+}
+var userLocation = new Place({name: 'Your Current Location'});
 var map;  // Main map
 var markers = [];  // Generated markers; every marker generated is added here
 var infoClosed = false;  // Keeps track of whether or not an infoWindow has been closed
@@ -29,14 +32,17 @@ function initMap() {
   // Function called when navigator.geolocation.getCurrentPosition succeeds
   function success(position) {
     var coords = position.coords;
-    userLocation.geometry.location.lat = function() {
-      return coords.latitude;
-    };
-    userLocation.geometry.location.lng = function() {
-      return coords.longitude;
-    };
-    map.setCenter(new google.maps.LatLng(coords.latitude, coords.longitude));
-    makeMarker(userLocation);
+    userLocation = new Place({lat: coords.latitude, lng: coords.longitude});
+    // userLocation.geometry.location.lat = function() {
+    //   return coords.latitude;
+    // };
+    // userLocation.geometry.location.lng = function() {
+    //   return coords.longitude;
+    // };
+    if (markers.length === 0) {
+      map.setCenter(new google.maps.LatLng(coords.latitude, coords.longitude));
+    }
+    // makeMarkers(userLocation);
     console.log('Successfully obtained user geolocation data.');
   }
   // Try getting the user's current geolocation information
@@ -56,11 +62,10 @@ function initMap() {
   }
   // Force set center of the map again
   map.setCenter(mapCenter);
-  var bounds = new google.maps.LatLngBounds();
 
-  // Convenient function to generate a marker and place it on the map
-  function makeMarker(place) {
-    // console.log(place);
+  // Convenient function to generate markers and place them on the map
+  function makeMarkers(place) {
+    var bounds = new google.maps.LatLngBounds();
     // 1 mile is 1.609344 kilometers
     var largeInfowindow = new google.maps.InfoWindow();
     var lat = place.geometry.location.lat();  // latitude from the place service
@@ -69,61 +74,61 @@ function initMap() {
     var name = place.name;
     var stopCounter = 0;
     var wheelchairCounter = 0;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: new google.maps.LatLng(lat, lng),
-      title: `<p>${name}</p><p>${address}</p><p>MBTA stops information currently being processed. The card will refresh when the info is available.</p>`,
-      animation: google.maps.Animation.DROP
-    });
-    markers.push(marker);
-    bounds.extend(new google.maps.LatLng(lat, lng));
-    marker.addListener('click', function() {
-      if (infoClosed) {
-        largeInfowindow.setContent(`<div>${marker.title}</div>`);
-        largeInfowindow.open(map, marker);
-        currentlyOpened = true;
-      } else {
-        populateInfoWindow(this, largeInfowindow);
-      }
-    });
-    map.setCenter(new google.maps.LatLng(lat, lng));
-    // Use localforage to iterate through the cached MBTA stops data
-    // For each stops, check that it's within a mile of the marker's geo info
-    // If so, increment our main counter by 1
-    // If also it's wheelchair access, increment the wheelchair counter by 1
+    // Use localforage to iterate through the cached stops.
+    // Grab each value and check if the location is within our expected radius.
+    // If so, make a Place object and a marker for it. Then add the marker to the map.
+    // Add an event listener to the generated marker to open the large info window
+    // with the right information
     localforage.iterate(function(value, key, IterNumber) {
+      // Check radius condition
+      // Make Place object and marker
+      // Add event listeners
       var p1 = {lat: lat, lon: lng};
       var p2 = {lat: value.lat, lon: value.lng};
-      if (getDistanceBetweenTwoPoints(p1, p2) <= (1.609344/2)) {
+      var distance = getDistanceBetweenTwoPoints(p1, p2);
+      if (distance <= (1.609344/2)) {
         stopCounter += 1;
+        var stop = new Place({
+          name: value.name,
+          lat: value.lat,
+          lng: value.lng,
+          wheelchair_accessible: value.wheelchair_boarding,
+          formatted_address: `${distance.toFixed(2)} mile from ${place.name}.`
+        });
         if (value.wheelchair_boarding > 0) {
           wheelchairCounter += 1;
+          stop.message = "Wheelchair Accessible";
+        } else {
+          stop.message = "Not Wheelchair Accessible";
         }
+        var marker = new google.maps.Marker({
+          map: map,
+          position: new google.maps.LatLng(stop.geometry.location.lat(), stop.geometry.location.lng()),
+          title: `<p>${stop.name}</p><p>${stop.formatted_address}</p><p>${stop.message}</p>`,
+          animation: google.maps.Animation.DROP
+        });
+        markers.push(marker);
+        bounds.extend(new google.maps.LatLng(lat, lng));
+        marker.addListener('click', function() {
+          if (infoClosed) {
+            largeInfowindow.setContent(`<div>${marker.title}</div>`);
+            largeInfowindow.open(map, marker);
+            currentlyOpened = true;
+          } else {
+            populateInfoWindow(this, largeInfowindow);
+          }
+        });
       }
     }).then(function() {
-      // console.log("Done iterating");
-      var newTitle = `<p>${name}</p><p>${address}</p><p>${stopCounter} total stops within a 0.5 mile radius</p><p>${wheelchairCounter} with wheelchair boarding access.</p>`;
-      marker.setTitle(newTitle);
-      largeInfowindow.marker = marker;
-      if (infoClosed) {
-        // console.log('Inside the if statement: infowindow is closed.');
-        largeInfowindow.setContent(newTitle);
-        largeInfowindow.open(map, marker);
-        currentlyOpened = true;
-      } else {
-        if (currentlyOpened) {
-          // console.log('Made it to the else statement');
-          largeInfowindow.setMarker = null;
-          largeInfowindow.marker = marker;
-          largeInfowindow.setContent(newTitle);
-          largeInfowindow.open(map, marker);
-        } else {
-          largeInfowindow.setContent(newTitle);
-          largeInfowindow.open(map, marker);
-          currentlyOpened = true;
-        }
-      }
+      // Recenter the map
+      // Generate a div below the map with the number of stops found
+      // Add info wheelchair accessibility
+      map.setCenter(bounds.getCenter());
+      console.log(`Number of stops: ${stopCounter}; Wheelchairs: ${wheelchairCounter}.`);
+      // map.fitBounds(bounds);
+      console.log('Markers generated and added to global markers tracker');
     }).catch(function(error) {
+      console.log('An error occurred...');
       console.log(error);
     });
   }
@@ -131,7 +136,7 @@ function initMap() {
   function callback(results, status) {
     // console.log(results[0])
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-      makeMarker(results[0]);
+      makeMarkers(results[0]);
     } else {
       alert('Could not find any information about your search term.');
       return;
@@ -165,6 +170,7 @@ function initMap() {
   function hideMarkers() {
     for (let i=0; i<markers.length; i++) {
       markers[i].setMap(null);
+      markers.pop();
     }
   }
   // An event handler that grabs the submitted search text and passes it to pinPlace
@@ -174,12 +180,18 @@ function initMap() {
     // map.setZoom(13);
     var searchText = document.querySelector("#searchtxt").value;
     if (searchText === "" || !searchText) {
-      alert("Cannot submit form with empty search text.");
-      return;
+      // alert("Cannot submit form with empty search text.");
+      // return;
+      if (! userLocation.geometry.location.lat() && !userLocation.geometry.location.lng()) {
+        alert("The application is probably busy retrieving your current location. Please try again later.");
+        return;
+      }
+      makeMarkers(userLocation);
+    } else {
+      pinPlace(searchText);
     }
-    pinPlace(searchText);
   });
-  map.fitBounds(bounds);
+  // map.fitBounds(bounds);
 }
 
 // The following has nothing to do with the map above.
